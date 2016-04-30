15947d882e8783fa2a99b3f1487412a8545c0cf6
dsclose <dsclose@gmail.com>
2016-04-30 12:39:10 +0100

    Added test for controller properties.

--- restbook/tests.py
+++ restbook/tests.py
@@ -8,15 +8,21 @@ from . import controller
 
 ###############################################################################
 
-class ControllerTest(TestCase):
+class _AbstractControllerTest:
     '''
-    Functioinal tests for a controller module.
+    Abstract testcase for any controller based tests.
     '''
 
     def setUp(self):
         self.controller = controller
 
-##############################
+
+###############################################################################
+
+class ControllerFunctionalTest(_AbstractControllerTest, TestCase):
+    '''
+    Functional tests for a controller module.
+    '''
 
     @given(name=text(), description=text())
     def test_can_store_restaurants(self, name, description):
@@ -34,4 +40,23 @@ class ControllerTest(TestCase):
 
         assert(unique_id is not None)
 
+
+###############################################################################
+
+class ControllerUnitTest(_AbstractControllerTest, TestCase):
+    '''
+    Unit tests for the controller module
+    '''
+
+    def test_has_expected_attributes(self):
+        feeback = 'Controller module "{module}" should have attribute "{attr}"'
+        expected = (
+            'restaurant_create',
+        )
+
+        for attribute in expected:
+            self.assertTrue(
+                hasattr(self.controller, attribute),
+                feeback.format(module=self.controller.__name__, attr=attribute)
+            )
 
