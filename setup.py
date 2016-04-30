125141a413f983287f82d3791595ad1bf3846e6c
dsclose <dsclose@gmail.com>
2016-04-30 13:11:33 +0100

    Switched to nose test collector.

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ setup(
     version='0.1',
     description='Manages restaurant bookings and seating plans.',
     packages=['restbook'],
+    test_suite='nose.collector',
     install_requires=[],
     tests_require=[
         'hypothesis',
+        'nose',
     ],
-    test_suite='restbook.tests'
 )
