dfaf2da2a832f26dfec61b1efba215849620e6c5
dsclose <dsclose@gmail.com>
2016-05-02 02:21:20 +0100

    Extended functional tests for storing restaurants to include tables.

--- restbook/tests/functional_tests.py
+++ restbook/tests/functional_tests.py
@@ -2,7 +2,7 @@
 from unittest import TestCase
 
 from hypothesis import assume, given
-from hypothesis.strategies import text
+from hypothesis.strategies import integers, lists, text
 
 from restbook import controller
 from restbook.tests import strategies
@@ -22,13 +22,15 @@ class ControllerFunctionalTest(TestCase):
     @given(
         name=text(),
         description=text(),
-        opening_times=strategies.opening_times
+        opening_times=strategies.opening_times,
+        tables=lists(integers(min_value=1, max_value=12))
     )
     def test_can_store_restaurants_with_opening_times(
         self,
         name,
         description,
-        opening_times
+        opening_times,
+        tables
     ):
         '''
         We should be able to create and retreive restaurants.
@@ -39,7 +41,8 @@ class ControllerFunctionalTest(TestCase):
         unique_id = self.controller.restaurant_create(
             name=name,
             description=description,
-            opening_times=opening_times
+            opening_times=opening_times,
+            tables=tables
         )
 
         assert(unique_id is not None)
@@ -48,6 +51,6 @@ class ControllerFunctionalTest(TestCase):
 
         assert(restaurant.name == name)
         assert(restaurant.description == description)
-
         assert(list(restaurant.opening_times) == list(opening_times))
+        assert(list(restaurant.tables) == list(tables))
 
